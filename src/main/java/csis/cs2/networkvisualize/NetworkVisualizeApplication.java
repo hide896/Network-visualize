5452db3f68053fe7045ce7dd6094cbdfba511393
Keita Morisaki <kmns.y.love@icloud.com>
2019-11-27 17:05:53 +0900

    first commit

---
+++ src/main/java/csis/cs2/networkvisualize/NetworkVisualizeApplication.java
@@ -0,0 +1,13 @@
+package csis.cs2.networkvisualize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkVisualizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkVisualizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csis/cs2/networkvisualize/NetworkVisualizeApplicationTests.java
@@ -0,0 +1,13 @@
+package csis.cs2.networkvisualize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkVisualizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
