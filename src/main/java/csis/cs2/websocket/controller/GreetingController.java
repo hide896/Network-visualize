9c799f224e33a31f9ce10c36b03205ce6261bd80
Keita Morisaki <kmns.y.love@icloud.com>
2019-12-03 13:37:10 +0900

    [update] Websocketで通信する機能を実装

--- src/main/java/csis/cs2/websocket/controller/GreetingController.java
+++ src/main/java/csis/cs2/websocket/controller/GreetingController.java
@@ -2,18 +2,22 @@ package csis.cs2.websocket.controller;
 
 import csis.cs2.websocket.entity.Greeting;
 import csis.cs2.websocket.entity.HelloMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+@Slf4j
 @Controller
 public class GreetingController {
     @MessageMapping("/hello")
-    @SendTo("/topic/greeting")
+    @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) {
+        log.info("[START]");
         Greeting tmp = new Greeting();
         tmp.setContent("Hello, " + HtmlUtils.htmlEscape(message.getName()));
+        log.info("[END]");
         return tmp;
     }
 }
