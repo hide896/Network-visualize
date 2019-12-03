1fa764502eee0b7e1cce110dd44dc51a7cd9c85b
Keita Morisaki <kmns.y.love@icloud.com>
2019-12-03 15:20:35 +0900

    [update] apiを叩いて別のところからwebsocketの通信を起こす

--- src/main/java/csis/cs2/websocket/controller/DemoController.java
+++ src/main/java/csis/cs2/websocket/controller/DemoController.java
@@ -3,24 +3,32 @@ package csis.cs2.websocket.controller;
 import csis.cs2.websocket.entity.Greeting;
 import csis.cs2.websocket.entity.HelloMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.converter.SimpleMessageConverter;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
 
 @Slf4j
-@Controller
-public class GreetingController {
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) {
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping
+    public void greeting(@RequestBody HelloMessage message) {
         log.info("[START]");
+        log.info("message.name = {}", message.getName());
         Greeting tmp = new Greeting();
         tmp.setContent("Hello, " + HtmlUtils.htmlEscape(message.getName()));
+        simpMessagingTemplate.convertAndSend("/topic/greetings", tmp);
         log.info("[END]");
-        return tmp;
     }
 }
