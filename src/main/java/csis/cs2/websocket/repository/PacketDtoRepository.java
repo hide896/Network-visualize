e16a9851c33e57270fdf6a2db58b657c4b8339ca
Keita Morisaki <kmns.y.love@icloud.com>
2020-01-06 15:23:12 +0900

    [update] 文字列をPacketDtoに変換するメソッドを作成

---
+++ src/main/java/csis/cs2/websocket/repository/PacketDtoRepository.java
@@ -0,0 +1,41 @@
+package csis.cs2.websocket.repository;
+
+import csis.cs2.websocket.entity.PacketDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Repository
+public class PacketDtoRepository {
+    public List<PacketDto> getPacketDtoFromString(List<String> strings) {
+        if(strings == null) {
+            log.error("Received null");
+            return null;
+        }
+        if(strings.size() == 0) {
+            log.error("Received an empty list");
+            return null;
+        }
+        List<PacketDto> packetDtoList = new ArrayList<PacketDto>();
+        for(String string : strings) {
+            String[] packetElemString = string.split(",");
+            if(packetElemString.length == 0) {
+                log.error("PacketListStrings contain empty element(s)");
+                continue;
+            }
+            PacketDto tmpPacketDto = new PacketDto();
+            tmpPacketDto.setSourceIp(packetElemString[0]);
+            tmpPacketDto.setDestinationIp(packetElemString[1]);
+            tmpPacketDto.setSourcePort(Integer.parseInt(packetElemString[2]));
+            tmpPacketDto.setDestinationPort(Integer.parseInt(packetElemString[3]));
+            if(packetElemString.length > 4) {
+                tmpPacketDto.setStringFlag(packetElemString[4]);
+            }
+            packetDtoList.add(tmpPacketDto);
+        }
+        return packetDtoList;
+    }
+}
